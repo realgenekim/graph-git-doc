efe6eda701e279a4d647a7e4ff5c12e48f08cda6
Gene Kim <genek@realgenekim.me>
2019-04-02 13:59:11 -0700

    project.clj dependency updates; clojure-1.10-1-beta

--- project.clj
+++ project.clj
@@ -3,12 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1-beta1"]
+                 ;[metasoarous/oz "1.5.7-SNAPSHOT"]
                  [metasoarous/oz "1.5.6"]
                  [org.clojure/data.csv "0.1.4"]
                  [clj-jgit "0.8.10"]
                  [clojure.java-time "0.3.2"]
-                 [archaeologist "0.1.1"]])
+                 [archaeologist "0.1.1"]]
 
                  ;[planisphere "0.1.9"]]
-  ;:repl-options {:init-ns graph-git-doc.manuscript-lines})
+  :repl-options {:init-ns graph-git-doc.manuscript-lines})
