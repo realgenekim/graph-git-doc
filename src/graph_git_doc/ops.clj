714913ad5f4ae2229b5e3addd96a2e208f3a409c
Gene Kim <>
2021-09-05 21:41:33 -0700

    create-commits! replaces all assignments in ops

--- src/graph_git_doc/ops.clj
+++ src/graph_git_doc/ops.clj
@@ -43,9 +43,10 @@
 
 
 ;(def commits (glog/gen-commit-hash-to-all-diffs))
-(glog/run-git-command "../test-git-repo" "manuscript.md" "git-log.txt")
-(def commits (->> (glog/list-of-commits "git-log.txt")
-                  (glog/add-change-commit-info)))
+(comment
+  (glog/run-git-command "../test-git-repo" "manuscript.md" "git-log.txt")
+  (def commits (->> (glog/list-of-commits "git-log.txt")
+                    (glog/add-change-commit-info))))
 
 (comment
   commits
@@ -57,9 +58,29 @@
 ;   add the word count and number of lines
 ;
 
-(def commits (->> commits
-                  (map #(gw/add-word-stats-to-git-commit! % "../test-git-repo" "manuscript.md"))
-                  (map #(gw/add-timestamp-to-git-commit! % "../test-git-repo"))))
+(comment
+  (def commits (->> commits
+                    (map #(gw/add-word-stats-to-git-commit! % "../test-git-repo" "manuscript.md"))
+                    (map #(gw/add-timestamp-to-git-commit! % "../test-git-repo")))))
+
+(defn create-commits!
+  []
+  (let [repo-dir "../test-git-repo"
+        manuscript "manuscript.md"
+        outfile "git-log.txt"
+
+        out (glog/run-git-command repo-dir manuscript outfile)
+
+        commit1 (->> (glog/list-of-commits outfile)
+                     (glog/add-change-commit-info))
+
+        commit2 (->> commit1
+                  (map #(gw/add-word-stats-to-git-commit! % repo-dir manuscript))
+                  (map #(gw/add-timestamp-to-git-commit! % repo-dir)))]
+
+    commit2))
+
+(def commits (create-commits!))
 
 (comment
   (->> commits
